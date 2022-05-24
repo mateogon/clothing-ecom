d2de6b7cb0854ef6ef93832ef5fed83bc1ef53fe
mateogon <mateogon97@hotmail.com>
2022-05-24 15:33:43 -0400

    updated menu-items to navigate with withNavigate

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -46,8 +46,8 @@ class Directory extends React.Component{
     render (){
         return <div className = 'directory-menu'>
             {
-                this.state.sections.map(({title,imageUrl,id, size}) => (
-                    <MenuItem key = {id} title= {title} imageUrl = {imageUrl} subtitle = "SHOP NOW" size = {size}></MenuItem>
+                this.state.sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key = {id} subtitle = "SHOP NOW" {...otherSectionProps}></MenuItem>
                 ))
             }
         </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import './menu-item.styles.scss'
-const MenuItem = ({title , imageUrl, subtitle,size}) => (
-            <div className = {`${size} menu-item`}>
-                <div className = 'background-image' style = {{
-                backgroundImage: `url(${imageUrl})`
-            }} ></div>
-                <div className = 'content'>
-                    <h1 className ='title'>{title.toUpperCase()}</h1>
-                    <h1 className ='subtitle'>{subtitle}</h1>
-                </div>
+const MenuItem = ({title ,linkUrl, imageUrl, subtitle,size}) => {
+    const navigate = useNavigate();
+    return (
+        <div className = {`${size} menu-item`} onClick={() => {navigate(linkUrl);}} >
+            <div className = 'background-image' style = {{
+            backgroundImage: `url(${imageUrl})`
+        }} ></div>
+            <div className = 'content'>
+                <h1 className ='title'>{title.toUpperCase()}</h1>
+                <h1 className ='subtitle'>{subtitle}</h1>
             </div>
+        </div>
 );
+}
 
 export default MenuItem;
